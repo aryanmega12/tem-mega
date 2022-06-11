842694ed8e17293f05091a4070fa9cde52291e7d
aryanmega12 <106868248+aryanmega12@users.noreply.github.com>
2022-06-11 04:31:55 +0430

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,10 +3,10 @@ read -p " Does you want to install aryan-mega-trn Shell in Termux?(Yes/No) : " i
 
 if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
     clear
-    cp bash.bashrc $HOME
+    cp bash.bashrc2 $HOME
     cd /data/data/com.termux/files/usr/etc
     rm -rf motd
-    rm -rf bash.bashrc
+    rm -rf bash.bashrc2
     cd $HOME
     mv bash.bashrc /data/data/com.termux/files/usr/etc
     echo -e "\e[1;91mInstall Shod dadash"
